627c025312d1898dfe80ab606762e661d3f9aea1
DenisSvinkovskij <1911.denis55@gmail.com>
2020-09-08 09:21:29 +0300

    done hw

---
+++ task-for-game.js
@@ -0,0 +1,13 @@
+const showTag = function () {
+  console.log('showTag -> this', this);
+  console.log('showTag -> this.tag', this.tag);
+};
+
+const user = {
+  tag: 'Mango',
+};
+
+user.showUserTag = showTag;
+console.log('user', user);
+
+user.showUserTag();
